96efac881d244615fa597e9ea5181fd6bfc5d242
Gimblet <gimbletdev@gmail.com>
2024-03-03 20:13:24 -0500

    Starting Deference Operator
    Been busy but im undestanding how the basic pointers work, Next step is to learn about the seccion of the deference operator
    Link : https://cplusplus.com/doc/tutorial/pointers/

--- Compound Data Types/Pointers.cpp
+++ Compound Data Types/Pointers.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+//! Pointers
+//! https://cplusplus.com/doc/tutorial/pointers/
+
+// When using pointers we have to know two new operators: (* and &)
+// In the case of the ampersand (&) it is used to get the address of a variable
+// However when using the asterisk (*) it is used to declare a pointer (Only a pointer can be directly assigned to the address of a variable)
+
+//? Adress-of Operator && Reference Operator
+
+inline void getAdress(int *ptr);
+
+inline void setAdress()
+{
+  int var = 20;
+  int *number = &var; // Declaring pointer and giving it the address of var
+  getAdress(number);
+  cout << var << endl; // 50 (Changed the value of var without directly using the variable)
+}
+
+//? Deference Operator
+
+inline void getAdress(int *ptr)
+{
+  *ptr = 50; // Deference Operator
+}
+
 int main()
 {
+  setAdress();
   return 0;
 }
