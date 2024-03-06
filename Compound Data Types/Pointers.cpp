8b4ffa1a23cbf058a550cdb69263bdbe602c8ac4
Gimblet <gimbletdev@gmail.com>
2024-03-05 22:21:36 -0500

    Declaring Pointers
    Resume coding at Pointers and Arrays
    doc: https://cplusplus.com/doc/tutorial/pointers/

--- Compound Data Types/Pointers.cpp
+++ Compound Data Types/Pointers.cpp
@@ -22,14 +22,42 @@ inline void setAdress()
 }
 
 //? Deference Operator
+// It refers to the value of the variable that the pointer is pointing to. The syntax for this is the asterisk (*) symbol followed by the pointer name.
 
 inline void getAdress(int *ptr)
 {
   *ptr = 50; // Deference Operator
 }
 
+//? Declaring Pointers
+// We have already seen declaration of pointers in the first functions. But here we will go in depth.
+
+inline void declarePointers()
+{
+  int number = 20, age = 10;
+  double extended = 20.5;
+  int *ptr;     // Declaring a pointer that is pointing to an integer
+  double *ptr2; // Declaring a pointer that is pointing to a double
+
+  //* A pointer may be used in more than one variable
+  ptr = &number;
+  *ptr = 10; //! Changing the value of number to 10
+  ptr = &age;
+  *ptr = 20; //! Changing the value of age to 20
+
+  ptr2 = &extended;
+  *ptr2 = *ptr; //! Changing the value of extended to the value of ptr
+
+  cout << "\nDeclaring Pointers" << endl;
+
+  cout << "\nNumber : " << number << endl; // 10
+  cout << "Age : " << age << endl;         // 20
+  cout << "Extended : " << *ptr2 << endl;
+}
+
 int main()
 {
   setAdress();
+  declarePointers();
   return 0;
 }
