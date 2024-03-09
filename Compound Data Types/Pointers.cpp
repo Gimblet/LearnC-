ca46eb59506cb65bd97801ce728746a6298a0197
Gimblet <gimbletdev@gmail.com>
2024-03-08 20:22:01 -0500

    Pointers Arit

--- Compound Data Types/Pointers.cpp
+++ Compound Data Types/Pointers.cpp
@@ -95,6 +95,60 @@ inline void arraysAndPointers()
   }
 }
 
+//? Pointer Initialization
+/*
+  Pointers might be intialized in the very first moment they are declared.
+
+  int *ptr = &var;
+
+  Pointers can be initialized to another pointer, array or address of a variable.
+*/
+
+//? Pointer Arithmetics
+
+// We have to take into consideration that arithmetic operations on pointers are different from those of regular types.
+//* Only addition and subtraction operations are allowed on pointers, and the result of these operations are highly influenced by the type's size on bytes. (The size might vary depending on the os)
+
+/*
+  If we supose that the size of a char is 1 byte, the size of a short is 2 bytes and the size of a long is 4 bytes, when doing increment the result on the adress would be as follows:
+
+  char *charPtr;
+  short *shortPtr;
+  long *longPtr;
+
+  ++charPtr; // Equals charPtr + 1
+  ++shortPtr; // Equals shortPtr + 2
+  ++longPtr; // Equals longPtr + 4
+*/
+
+void pointerArit()
+{
+  int *p, *q;
+  cout << "\nPointer Arithmetics" << endl;
+
+  // From this point on we need to know the rule of prescende for the suffixes and prefixes of the ++ and -- operators.
+  //! In a prefix, the increment is done BEFORE the evaluation of the expression, while in a suffix the increment is done AFTER the evaluation of the expression. Therefore:
+
+  *p++;   //* same as *(p++): increment pointer, and dereference unincremented address
+  *++p;   //* same as *(++p): increment pointer, and dereference incremented address
+  ++*p;   //* same as ++(*p): dereference pointer, and increment the value it points to
+  (*p)++; //* dereference pointer, and post-increment the value it points to
+
+  // In the first case, the pointer is incremented first, however as the dereference operator is a suffix, the value of the pointer is the one before the increment.
+  // In the second case, the pointer is incremented first, and then the value of the pointer is the one after the increment.
+  // In the third case, the value of the pointer is deferenced. With the value now gotten, it is incremented by one.
+  // In the fourth case, the value of the pointer is dereferenced and then incremented by one.
+
+  //! A often used case with incremeted pointers:
+
+  *p++ = *q++;
+  /*
+    Equals to: *p = *q;
+    p++;
+    q++;
+  */
+}
+
 //! The brackets in the arrays are a some sort of dereference operator called the subscript operator. It is used to get the value of the variable that the pointer is pointing to.
 
 int main()
